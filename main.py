48fe17ebab257099f186e7263c701d6928f4db83
raqkrus <148219186+raqkrus@users.noreply.github.com>
2023-11-15 21:43:41 -0500

    background works

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ while running:
 
     screen.blit(build_background(), (0,0))
 
-    #WORKS ^^^^
 
     # set color for table and shelves
     light_brown = (196, 164, 132)
@@ -51,10 +50,59 @@ while running:
     left_shelf_x = 0
     right_shelf_x = screen_width - shelf_width
 
+    # draw shelves on screen
+    pygame.draw.rect(screen, lighter_brown, (left_shelf_x, shelf_y, shelf_width, shelf_height))
+    pygame.draw.rect(screen, lighter_brown, (right_shelf_x, shelf_y, shelf_width, shelf_height))
+
+    # set up shelf dividers
+    divider_width = 10
+    divider_height = 200
+    divider_y = 200
+
+    # set vertical divider x positions
+    left_divider_x = shelf_width / 2
+    right_divider_x = screen_width - (shelf_width / 2)
+
+    # draw vertical dividers on screen
+    pygame.draw.rect(screen, light_brown, (left_divider_x, divider_y, divider_width, divider_height))
+    pygame.draw.rect(screen, light_brown, (right_divider_x, divider_y, divider_width, divider_height))
+
+    # set up horizontal shelf dividers
+    h_divider_width = 200
+    h_divider_height = 10
+    h_divider_y = 300
+
+    # set divider x positions
+    left_h_divider_x = 0
+    right_h_divider_x = screen_width - shelf_width
+
+    # draw horizontal dividers on screen
+    pygame.draw.rect(screen, light_brown, (left_h_divider_x, h_divider_y, h_divider_width, h_divider_height))
+    pygame.draw.rect(screen, light_brown, (right_h_divider_x, h_divider_y, h_divider_width, h_divider_height))
 
 
+    class Toppings(pygame.sprite.Sprite):
+        def __init__(self, screen, image, initial_position, scale_factor):
+            super().__init__()
 
+            self.image = pygame.image.load(image)
+            self.image = pygame.transform.scale(self.image, (
+                int(self.image.get_width() * scale_factor), int(self.image.get_height() * scale_factor)))
 
+            self.rect = self.image.get_rect()
+            self.rect.center = initial_position
+            self.screen = screen
+
+        def draw(self, screen):
+            screen.blit(self.background, (0, 0))
+            screen.blit(self.image, self.rect.topleft)
+
+            toppings_info = [
+                {"image_path": "images/candy.blue.png",
+                 "initial_position": (screen.get_width() // 2, screen.get_height() * 2 // 3),
+                 "scale_factor": 0.5}
+
+            ]
 
 
     pygame.display.update()
