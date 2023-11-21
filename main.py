4f73b104a40ba9f718ac143e491f8578992448c8
raqkrus <148219186+raqkrus@users.noreply.github.com>
2023-11-21 09:43:23 -0500

    allows you to click blue and red toppings (yellow is broken) and add to cake.

--- main.py
+++ main.py
@@ -7,51 +7,57 @@ running = True
 screen_width = 800
 screen_height = 600
 
+
 def welcome_screen():
-    screen.fill((200,162,200))
+    screen.fill((200, 162, 200))
     font = pygame.font.Font(None, 36)
-    text1 = font.render("Chose a number of Layers for your Cake!", True, (255,255,255),(200,162,200))
-    text2 = font.render("Welcome to the Bakery!", True, (255,255,255),(200,162,200))
-    button1text = font.render("1", True, (204, 0, 102),(170,255,195))
+    text1 = font.render("Chose a number of Layers for your Cake!", True, (255, 255, 255), (200, 162, 200))
+    text2 = font.render("Welcome to the Bakery!", True, (255, 255, 255), (200, 162, 200))
+    button1text = font.render("1", True, (204, 0, 102), (170, 255, 195))
     button2text = font.render("2", True, (204, 0, 102), (170, 255, 195))
-    button3text = font.render("3", True, (204, 0, 102),(170,255,195))
+    button3text = font.render("3", True, (204, 0, 102), (170, 255, 195))
     button_width = 100
     button_height = 50
     number_of_buttons = 3
     distance_from_bottom = 50
-    button1_pos = (200-button_width/2,400)
-    button2_pos = (400-button_width/2, 400)
-    button3_pos = (600-button_width/2, 400)
-    pygame.draw.rect(screen, (170, 255, 195), (button1_pos, (button_width, button_height))) # Replace with screen.blit("image", button1_pos)
+    button1_pos = (200 - button_width / 2, 400)
+    button2_pos = (400 - button_width / 2, 400)
+    button3_pos = (600 - button_width / 2, 400)
+    pygame.draw.rect(screen, (170, 255, 195),
+                     (button1_pos, (button_width, button_height)))  # Replace with screen.blit("image", button1_pos)
     pygame.draw.rect(screen, (170, 255, 195), (button2_pos, (button_width, button_height)))
     pygame.draw.rect(screen, (170, 255, 195), (button3_pos, (button_width, button_height)))
-    screen.blit(text1, (screen_width//2 - text1.get_width() // 2, screen_height // 2 - text1.get_height()//2))
+    screen.blit(text1, (screen_width // 2 - text1.get_width() // 2, screen_height // 2 - text1.get_height() // 2))
     screen.blit(text2, (screen_width // 2 - text2.get_width() // 2, screen_height // 3 - text2.get_height() // 2))
-    screen.blit(button1text, (240-button_width/2,410))
+    screen.blit(button1text, (240 - button_width / 2, 410))
     screen.blit(button2text, (440 - button_width / 2, 410))
     screen.blit(button3text, (640 - button_width / 2, 410))
     start_game = False
     while start_game == False:
         mouse = pygame.mouse.get_pos()
         pygame.event.get()
-        #If over button 1
-        if button1_pos[0] < mouse[0] < button1_pos[0] + button_width and button1_pos[1] < mouse[1] < button1_pos[1] + button_height:
+        # If over button 1
+        if button1_pos[0] < mouse[0] < button1_pos[0] + button_width and button1_pos[1] < mouse[1] < button1_pos[
+            1] + button_height:
             print("Over Button 1!")
-            if pygame.mouse.get_pressed()[0]: #0 and 1 for x and y pos, 0 for left mouse click
+            if pygame.mouse.get_pressed()[0]:  # 0 and 1 for x and y pos, 0 for left mouse click
                 start_game = True
                 return 1
-        if button2_pos[0] < mouse[0] < button2_pos[0] + button_width and button2_pos[1] < mouse[1] < button2_pos[1] + button_height:
+        if button2_pos[0] < mouse[0] < button2_pos[0] + button_width and button2_pos[1] < mouse[1] < button2_pos[
+            1] + button_height:
             print("Over Button 2!")
-            if pygame.mouse.get_pressed()[0]: #0 and 1 for x and y pos, 0 for left mouse click
+            if pygame.mouse.get_pressed()[0]:  # 0 and 1 for x and y pos, 0 for left mouse click
                 start_game = True
                 return 2
-        if button3_pos[0] < mouse[0] < button3_pos[0] + button_width and button3_pos[1] < mouse[1] < button3_pos[1] + button_height:
+        if button3_pos[0] < mouse[0] < button3_pos[0] + button_width and button3_pos[1] < mouse[1] < button3_pos[
+            1] + button_height:
             print("Over Button 3!")
-            if pygame.mouse.get_pressed()[0]: #0 and 1 for x and y pos, 0 for left mouse click
+            if pygame.mouse.get_pressed()[0]:  # 0 and 1 for x and y pos, 0 for left mouse click
                 start_game = True
                 return 3
 
         pygame.display.update()
+
 
 def build_background():
     background_image = pygame.image.load("images/purple_background.jpg")
@@ -62,9 +68,6 @@ def build_background():
 # get screen size/width
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pygame Background")
-
-# load the image
-# background_image = "images/purple_background.jpg"
 
 # set color for table and shelves
 light_brown = (196, 164, 132)
@@ -105,12 +108,6 @@ left_h_divider_x = 0
 right_h_divider_x = screen_width - shelf_width
 
 # dictionary for toppings
-toppings_info = [
-    {"image_path": "images/candyBlue.png",
-     "initial_position": (screen.get_width() // 2, screen.get_height() * 2 // 3),
-     "scale_factor": 0.5}
-
-]
 
 toppings = pygame.sprite.Group()
 
@@ -130,33 +127,35 @@ class Toppings(pygame.sprite.Sprite):
         def draw(self):
             self.screen.blit(self.image, self.rect.topleft)
 
-        self.toppings_sprites = []
-        for topping in toppings_info:
-            self.toppings_sprites.append(
-                Toppings(screen, topping["image_path"], topping["initial_position"], topping["scale_factor"])
-            )
-
 def draw_cake_layer(number_of_tiles, layer_number):
     center_of_screen = screen_width / 2
     # Draw the left and right of the layer
-    screen.blit(cake_left, (center_of_screen - (number_of_tiles/2) * tile_length, table_height - tile_length * layer_number))
-    screen.blit(cake_right, (center_of_screen + (number_of_tiles/2 - 1) * tile_length, table_height - tile_length * layer_number))
+    screen.blit(cake_left,
+                (center_of_screen - (number_of_tiles / 2) * tile_length, table_height - tile_length * layer_number))
+    screen.blit(cake_right,
+                (center_of_screen + (number_of_tiles / 2 - 1) * tile_length, table_height - tile_length * layer_number))
     # Draw the middle of the layer
-    for tile in range(int((number_of_tiles - 2)/2)):  #-2 bc we already drew two tiles, divided by 2 bc were drawing two at same time
-        screen.blit(cake_middle, (center_of_screen - (tile + 1) * tile_length, table_height - tile_length * layer_number))  # Middle tiles on the left of the screen
-        screen.blit(cake_middle, (center_of_screen + (tile) * tile_length, table_height - tile_length * layer_number))  # Middle tiles on the right of the screen
+    for tile in range(int((
+                                  number_of_tiles - 2) / 2)):  # -2 bc we already drew two tiles, divided by 2 bc were drawing two at same time
+        screen.blit(cake_middle, (center_of_screen - (tile + 1) * tile_length,
+                                  table_height - tile_length * layer_number))  # Middle tiles on the left of the screen
+        screen.blit(cake_middle, (center_of_screen + (tile) * tile_length,
+                                  table_height - tile_length * layer_number))  # Middle tiles on the right of the screen
+
 
 number = welcome_screen()
 
 topping = False  # I will remove this once I can draw toppings in the class
 
 topping_positions = []
+current_topping = None
+
+event = None
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
 
     screen.blit(build_background(), (0, 0))
 
@@ -184,7 +183,7 @@ while running:
     pinkcream = pygame.image.load("images/creamPink.png")
     vanillacream = pygame.image.load("images/creamVanilla.png")
     heart = pygame.image.load("images/heart.png")
-    #load in other candy types here, then blit like below on other parts of shelf!
+    # load in other candy types here, then blit like below on other parts of shelf!
 
     # Draw toppings on shelves
     screen.blit(candy_blue, (20, 230))
@@ -204,16 +203,31 @@ while running:
         # Top Layer
         draw_cake_layer(2, 3)
 
-    if pygame.key.get_pressed()[pygame.K_SPACE]:
-        #toppings.add(Toppings(screen, "images/candyBlue.png", (200,200), 1))
+
+    if event is not None and event.type == pygame.MOUSEBUTTONDOWN:
+        # toppings.add(Toppings(screen, "images/candyBlue.png", (200,200), 1))
         position = pygame.mouse.get_pos()
-        position = (position[0] - candy_blue.get_width()/2, position[1] - candy_blue.get_height()/2) #positions candy to center of mouse click
-        topping_positions.append(position)
+        position = (position[0] - candy_blue.get_width() / 2, position[1] - candy_blue.get_height() / 2) #where should this goooo?
 
-    for i in topping_positions:
-        print(topping_positions)
-        screen.blit(candy_blue, i)
+        #issues here
+        if 0 <= position[0] <= candy_blue.get_width() and 230 <= position[1] <= 280:
+            print("over blue")
+            current_topping = candy_blue
 
+        elif 0 <= position[0] <= candy_red.get_width() and 330 <= position[1] <= 380:
+            print("over red")
+            current_topping = candy_red
+
+#yellow wont work
+        elif 120 <= position[0] <= candy_yellow.get_width() and 230 <= position[1] <= 280:
+            print("over yellow")
+            current_topping = candy_yellow
+
+        elif current_topping is not None:
+            topping_positions.append((position, current_topping))
+
+    for pos, candy_image in topping_positions:
+        screen.blit(candy_image, pos)
 
     pygame.display.update()
 
